0eac9348ca791e6e47838cc4735950323bb85f4a
julioschuambach <julioschuambach.dev@gmail.com>
2023-10-13 19:29:55 -0300

    feat: implemented method to read sql scripts

--- DevChallenge/DevChallenge/Program.cs
+++ DevChallenge/DevChallenge/Program.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace DevChallenge
 {
     public class Program
@@ -7,9 +9,37 @@ namespace DevChallenge
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            MapEndpoints(app);
 
             app.Run();
+        }
+
+        private static void MapEndpoints(WebApplication app)
+        {
+            app.MapGet("/read-sql-scripts", () =>
+            {
+                List<string> paths = new()
+                {
+                    "..\\..\\Resources\\Scripts\\Schema.sql",
+                    "..\\..\\Resources\\Scripts\\Data.sql",
+                    "..\\..\\Resources\\Scripts\\2021-03-14-06-31_corrige_sigla_estado_roraima.sql"
+                };
+
+                StringBuilder sb = new();
+                foreach (var path in paths)
+                {
+                    sb.Append(File.ReadAllText(path));
+                }
+
+                try
+                {
+                    return Results.Ok(sb.ToString());
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            });
         }
     }
 }
