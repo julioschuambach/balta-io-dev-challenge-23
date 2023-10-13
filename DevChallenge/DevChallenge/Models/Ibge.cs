118d2035689306550d5363f475da176f82328d26
julioschuambach <julioschuambach.dev@gmail.com>
2023-10-13 19:19:02 -0300

    feat: created ibge model

---
+++ DevChallenge/DevChallenge/Models/Ibge.cs
@@ -0,0 +1,12 @@
+﻿namespace DevChallenge.Models
+{
+    public class Ibge
+    {
+        public string Id { get; private set; }
+        public string State { get; private set; }
+        public string City { get; private set; }
+
+        public Ibge(string id, string state, string city)
+            => (Id, State, City) = (id, state, city);
+    }
+}
