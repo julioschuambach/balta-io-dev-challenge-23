4b58fb49a2b436b151e897488cec394953f8ab8a
julioschuambach <julioschuambach.dev@gmail.com>
2023-10-13 21:41:28 -0300

    feat: implemented method to create database

--- DevChallenge/DevChallenge.Api/Program.cs
+++ DevChallenge/DevChallenge.Api/Program.cs
@@ -1,4 +1,7 @@
+using DevChallenge.Data.Contexts;
 using DevChallenge.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevChallenge
 {
@@ -7,11 +10,17 @@ namespace DevChallenge
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
             var app = builder.Build();
 
             MapEndpoints(app);
 
             app.Run();
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<DevChallengeDbContext>();
         }
 
         private static void MapEndpoints(WebApplication app)
@@ -23,6 +32,21 @@ namespace DevChallenge
                 try
                 {
                     return Results.Ok(script);
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            });
+
+            app.MapGet("/create-database", ([FromServices] DevChallengeDbContext context) =>
+            {
+                var script = ResourcesHelper.GetSqlScript();
+
+                try
+                {
+                    context.Database.ExecuteSqlRaw(script);
+                    return Results.Ok("Database created successfully!");
                 }
                 catch (Exception ex)
                 {
