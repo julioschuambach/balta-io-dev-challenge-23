983dc51d693b0cfd67f9883b9ea058c428fe5a71
julioschuambach <julioschuambach.dev@gmail.com>
2023-10-15 17:35:07 -0300

    fix: added max length for user properties on user mapping

--- DevChallenge/DevChallenge.Api/Data/Mappings/UserMapping.cs
+++ DevChallenge/DevChallenge.Api/Data/Mappings/UserMapping.cs
@@ -15,21 +15,25 @@ namespace DevChallenge.Api.Data.Mappings
             builder.Property(x => x.Id)
                    .HasColumnName("id")
                    .HasColumnType("CHAR")
+                   .HasMaxLength(36)
                    .IsRequired();
 
             builder.Property(x => x.Username)
                    .HasColumnName("username")
                    .HasColumnType("VARCHAR")
+                   .HasMaxLength(50)
                    .IsRequired();
 
             builder.Property(x => x.Password)
                    .HasColumnName("password")
                    .HasColumnType("VARCHAR")
+                   .HasMaxLength(50)
                    .IsRequired();
 
             builder.Property(x => x.Role)
                    .HasColumnName("role")
                    .HasColumnType("CHAR")
+                   .HasMaxLength(15)
                    .IsRequired();
 
             builder.HasIndex(x => x.Id, "IX_USER_Id")
