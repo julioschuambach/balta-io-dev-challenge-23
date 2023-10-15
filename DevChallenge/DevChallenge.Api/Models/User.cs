7ca8fbcb4090dd98a3d3e3a85af334c3705cec74
julioschuambach <julioschuambach.dev@gmail.com>
2023-10-15 10:34:40 -0300

    feat: created user model

---
+++ DevChallenge/DevChallenge.Api/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace DevChallenge.Api.Models
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; }
+
+        public User(string username, string password)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            Password = password;
+            Role = "user";
+        }
+    }
+}
